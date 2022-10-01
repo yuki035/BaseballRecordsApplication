75cd670d98dd1b714b39f2c38487faca5af6d9fd
yuki035 <yukiyuki1918@icloud.com>
2022-10-02 00:18:03 +0900

    add: つなぎ合わせたDataFramefから指標を計算してxlsxファイルを出力する機能を実装

--- teamRecord.py
+++ teamRecord.py
@@ -1,112 +1,120 @@
 import os
-# import openpyxl as px
+from pathlib import Path
+import openpyxl as px
 # from openpyxl.xml.constants import NAMESPACES
 import pandas as pd
 import glob
 # from openpyxl.styles import Alignment, PatternFill
-# import datetime
+import datetime
 
+# 成績を生成する選手を取得
+def get_players_name(path: str):
+    wb = px.load_workbook(path + "\選手登録.xlsx")
+    ws = wb.worksheets[0]
+    for col in ws.columns:
+        players_name = []
+        for cell in col:
+            players_name.append(cell.value)
+
+    return players_name
+
+# 打撃成績を計算
+def calc_bat_record(df: pd.DataFrame):
+    df['盗塁成功率'] = df['盗塁'] / df['盗塁企画']
+    df['打率'] = df['安打'] / df['打数']
+    divisor = df['打数'] + df['四球'] + df['死球'] + df['犠飛']
+    df['出塁率'] = (df['安打'] + df['四球'] + df['死球']) / divisor
+    df['長打率'] = df['塁打'] / df['打数']
+    df['OPS'] = df['出塁率'] + df['長打率']
+    df['BB/K'] = df['四球'] / df['三振']
+    wOBA_divided = 0.692*df['四球'] + 0.73*df['死球'] + 0.966*df['失策'] + 0.865 *df['単打'] + 1.334*df['二塁打'] + 1.725*df['三塁打'] + 2.065*df['本塁打']
+    df['wOBA'] = wOBA_divided / divisor
     
+    #欠損値を0で補完
+    df.fillna(0, inplace=True)
+    
+# 投手成績を計算
+def calc_pitch_record(df: pd.DataFrame):
+    # 投球回（Innings pitched / IP）
+    IP = df['奪アウト数']/3
+    # 奪アウト数　→　投球回数
+    q, mod = divmod(df['奪アウト数'], 3)
+    df['奪アウト数'] = q + 0.1*mod
+    df.rename(columns={'奪アウト数':'投球回'}, inplace=True)
+    df['防御率'] = df['自責点']*7 / IP
+    df['奪三振率'] = df['奪三振']*7 / IP
+    df['K%'] = df['奪三振'] / df['打者数']
+    df['BB%'] = df['与四球'] / df['打者数']
+    df['被打率'] = df['被安打'] / df['打者数']
+    df['WHIP'] = (df['与四球'] + df['被安打']) / IP
+    df['投球数/回'] = df['投球数']/IP
+    
+    #欠損値を0で補完
+    df.fillna(0, inplace=True)
+
+
 def main():
+    print("開始")
     # パスを設定
-    import_foloder_path = os.path.dirname(__file__) + '\試合結果'
-    export_folder_path = os.path.dirname(__file__) + '\チーム成績'
-    
+    dirname = os.path.dirname(__file__)
+    import_foloder_path = dirname + '\試合結果'
+    export_folder_path = dirname + '\チーム成績'
+
     # 試合結果のデータをdf_concatでつなぎ合わせる
     path = import_foloder_path + '\*.xlsx'
     file_paths = glob.glob(path)
     df_bat_concat = pd.DataFrame()
     df_pitch_concat = pd.DataFrame()
     for path in file_paths:
-        df_bat_read_excel   = pd.read_excel(path, sheet_name='打撃成績', index_col=0)
+        df_bat_read_excel = pd.read_excel(path, sheet_name='打撃成績', index_col=0)
         df_pitch_read_excel = pd.read_excel(path, sheet_name='投手成績', index_col=0)
-        df_bat_concat=pd.concat([df_bat_concat, df_bat_read_excel])
+        df_bat_concat = pd.concat([df_bat_concat, df_bat_read_excel])
         df_pitch_concat = pd.concat([df_pitch_concat, df_pitch_read_excel])
-        
 
+    # カラムに試合or登板を追加
+    df_bat_concat['試合'] = 1
+    df_pitch_concat['登板'] = 1
+
+    # 集計データを合計したデータフレームを作成
+    df_bat_sum = df_bat_concat[['試合', '打席', '打数', '安打', '単打', '二塁打', '三塁打', '本塁打', '塁打',
+                                '打点', '得点', '四球', '死球', '犠打', '犠飛', '打撃妨害', '失策', '野選',
+                                '振り逃げ', '三振', '併殺', '盗塁企画', '盗塁']].groupby('名前').sum()
+
+    df_pitch_sum = df_pitch_concat[['登板', '完封', '完投', '勝利', '敗戦', '引き分け', 'セーブ', '奪アウト数', '投球数', '打者数', '被安打',
+                                    '与四球', '与死球', '奪三振', '失点', '自責点']].groupby('名前').sum()
+
+    # 成績を生成する選手を抽出
+    players_name = get_players_name(dirname)
+    df_bat_sum = df_bat_sum.filter(items=players_name, axis=0)
+    df_pitch_sum = df_pitch_sum.filter(items=players_name, axis=0)
+
+    # チーム総合をデータ化
+    games = len(file_paths)
+
+    df_bat_sum.loc['チーム総合'] = df_bat_sum.iloc[0:, 1:].sum()
+    df_bat_sum.loc['チーム総合', '試合'] = games
+    df_pitch_sum.loc['チーム総合'] = df_pitch_sum.iloc[0:, 1:].sum()
+    df_pitch_sum.loc['チーム総合', '登板'] = games
+
+    # 指標計算
+    calc_bat_record(df_bat_sum)
+    calc_pitch_record(df_pitch_sum)
     
+    
+    
+    #一旦ファイルに出力する
+    date = str(datetime.date.today())
+    with pd.ExcelWriter(export_folder_path+'/通算_'+date+'.xlsx') as writer:
+        df_bat_sum.to_excel(writer, sheet_name='打撃成績')
+        df_pitch_sum.to_excel(writer, sheet_name='投手成績')
+
+    print("完了")
+
 if __name__ == '__main__':
     main()
 
 # export_file_team_path = 'C:/Users/Mori Yuki/Desktop/岡大軟式野球DB/チーム成績/通算'
 # import_folder_path = 'C:/Users/Mori Yuki/Desktop/岡大軟式野球DB/試合結果'
-
-
-
-
-
-
-# #データを新チーム用にする 
-# no_bat_player_name = ['塚本\u3000純平', '荒瀬\u3000平太郎', '藤川\u3000祐一', '東川\u3000知樹','大下\u3000凛',
-#                   '松岡\u3000潤樹', '増野\u3000颯太', '湊\u3000晴貴', '國岡\u3000雅樹', '難波\u3000弥凪', 'チーム総合','田村　知広']
-# no_pitch_player_name = ['藤川\u3000祐一', '塚本\u3000純平', '松岡\u3000潤樹', '湊\u3000晴貴', 'チーム総合']
-
-# df_bat_concat.drop(no_bat_player_name, inplace=True)
-# df_pitch_concat.drop(no_pitch_player_name, inplace=True)
-
-# # カラムに試合or登板を追加
-# df_bat_concat['試合']=1
-# df_pitch_concat['登板']=1
-
-# #集計データを合計したデータフレームを作成
-# df_bat_sum=df_bat_concat[['試合','打席', '打数', '安打', '単打', '二塁打', '三塁打', '本塁打', '塁打',
-#                   '打点', '得点', '四球', '死球', '犠打', '犠飛', '打撃妨害', '失策', '野選',
-#                   '振り逃げ', '三振', '併殺', '盗塁企画', '盗塁']].groupby('名前').sum()
-
-# df_pitch_sum=df_pitch_concat[['登板','完封', '完投', '勝利', '敗戦', '引き分け', 'セーブ', '奪アウト数', '投球数', '打者数', '被安打',
-#        '与四球', '与死球', '奪三振', '失点', '自責点']].groupby('名前').sum()
-
-
-# # チーム総合をデータ化
-# games = len(file_path)
-
-# df_bat_sum.loc['チーム総合'] = df_bat_sum.iloc[0: ,1:].sum()
-# df_bat_sum.loc['チーム総合','試合']=games
-
-# df_pitch_sum.loc['チーム総合'] = df_pitch_sum.iloc[0: ,1:].sum()
-# df_pitch_sum.loc['チーム総合','登板']=games
-
-
-
-# # 指標計算
-# # 打撃
-# df_bat_sum['盗塁成功率'] = df_bat_sum['盗塁'] / df_bat_sum['盗塁企画']
-# df_bat_sum['打率'] = df_bat_sum['安打']  / df_bat_sum['打数']
-# divisor = df_bat_sum['打数'] + df_bat_sum['四球'] + df_bat_sum['死球'] + df_bat_sum['犠飛']
-# df_bat_sum['出塁率'] = (df_bat_sum['安打'] + df_bat_sum['四球'] + df_bat_sum['死球']) / divisor
-# df_bat_sum['長打率'] = df_bat_sum['塁打'] / df_bat_sum['打数']
-# df_bat_sum['OPS'] = df_bat_sum['出塁率'] + df_bat_sum['長打率']
-# df_bat_sum['BB/K'] = df_bat_sum['四球'] / df_bat_sum['三振']
-# wOBA_divided = (0.692*df_bat_sum['四球'] + 0.73*df_bat_sum['死球'] + 0.966*df_bat_sum['失策'] + 0.865*df_bat_sum['単打'] + 1.334*df_bat_sum['二塁打'] + 1.725*df_bat_sum['三塁打'] + 2.065*df_bat_sum['本塁打'])
-# df_bat_sum['wOBA'] = wOBA_divided / divisor
-# # 投手
-# # 投球回（Innings pitched / IP）
-# IP = df_pitch_sum['奪アウト数']/3
-# # 奪アウト数　→　投球回数
-# q, mod = divmod(df_pitch_sum['奪アウト数'], 3)
-# df_pitch_sum['奪アウト数'] = q + 0.1*mod
-# df_pitch_sum.rename(columns={'奪アウト数':'投球回'}, inplace=True)
-
-# df_pitch_sum['防御率'] = df_pitch_sum['自責点']*7 / IP
-# df_pitch_sum['奪三振率'] = df_pitch_sum['奪三振']*7 / IP
-# df_pitch_sum['K%'] = df_pitch_sum['奪三振'] / df_pitch_sum['打者数']
-# df_pitch_sum['BB%'] = df_pitch_sum['与四球'] / df_pitch_sum['打者数']
-# df_pitch_sum['被打率'] = df_pitch_sum['被安打'] / df_pitch_sum['打者数']
-# df_pitch_sum['WHIP'] = (df_pitch_sum['与四球'] + df_pitch_sum['被安打']) / IP
-# df_pitch_sum['投球数/回'] = df_pitch_sum['投球数']/IP
-
-
-# #欠損値を0で補完
-# df_bat_sum.fillna(0, inplace=True)
-
-
-# #一旦ファイルに出力する
-# date = str(datetime.date.today())
-# with pd.ExcelWriter(export_file_team_path+'/通算_'+date+'.xlsx') as writer:
-#     df_bat_sum.to_excel(writer, sheet_name='打撃成績')
-#     df_pitch_sum.to_excel(writer, sheet_name='投手成績')
-
-
 
 # # 書式設定
 
@@ -153,7 +161,7 @@ if __name__ == '__main__':
 #     else:
 #         width = 8
 #     ws_bat.column_dimensions[bat_excel_number[i]].width = width
-    
+
 # for i in range(0, pitch_maxcol):
 #     if i == 0:
 #         width = 14
@@ -168,8 +176,8 @@ if __name__ == '__main__':
 # for i in range(24,bat_maxcol):
 #     for j in range(1,bat_maxrow+1):
 #         ws_bat[bat_excel_number[i]+str(j)].number_format = "0.000"
- 
-# # 投手       
+
+# # 投手
 # rate_format = "0.000"
 # for i in range(17,pitch_maxcol):
 #     if i == pitch_maxcol-1:
@@ -181,11 +189,11 @@ if __name__ == '__main__':
 # for i in range(1,bat_maxcol):
 #     for j in range(2,bat_maxrow+1):
 #         ws_bat[bat_excel_number[i]+str(j)].alignment = Alignment(horizontal = 'center')
-        
+
 # for i in range(1,pitch_maxcol):
 #     for j in range(2,pitch_maxrow+1):
 #         ws_pitch[excel_number[i]+str(j)].alignment = Alignment(horizontal = 'center')
-       
+
 # #ヘッダーを縦書きに設定
 # for i in range(1,bat_maxcol):
 #     ws_bat[bat_excel_number[i]+'1'].alignment = Alignment(vertical = 'center',textRotation = 255)
@@ -219,7 +227,7 @@ if __name__ == '__main__':
 # for i in range(0,bat_maxcol):
 #     cell = ws_bat[bat_excel_number[i]+str(bat_maxrow)]
 #     cell.fill = PatternFill(patternType = 'solid', fgColor=BAT_TITL_CELL_COLOR)
-    
+
 
 # for i in range(0,pitch_maxcol):
 #     cell = ws_pitch[excel_number[i]+'1']
