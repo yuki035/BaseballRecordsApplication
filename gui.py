8b73bc0f04569052b22a387e4ebcc14fb0ced981
yuki035 <yukiyuki1918@icloud.com>
2022-10-02 00:22:20 +0900

    update: ボタンタップ時の処理を変更

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter
+import teamRecord
 
 class Application(tkinter.Frame):
     def __init__(self, root=None):
@@ -18,7 +19,7 @@ class Application(tkinter.Frame):
         
         # チーム成績生成ボタン
         team_record_btn = tkinter.Button(self, text="チーム成績を生成",
-                                         command=self.test)
+                                         command=teamRecord.main)
         team_record_btn.place(x= 140, y = 100)
         
         # 個人成績生成ボタン
